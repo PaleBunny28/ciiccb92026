731a1bef86a501b2035a30ab0b31cad3cff4a24a
AngeloViloria <119723574+PaleBunny28@users.noreply.github.com>
2026-07-23 15:59:39 +0800

    Add Task2.java with main method and variable usage

---
+++ Task2.java
@@ -0,0 +1,17 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte b = 1;
+        short s = 0;
+        int i = 3110;
+        float f = 2.0f;
+        boolean bool = true;
+        char c1 = 'H';
+        char c2 = 'w';
+        char c3 = 'r';
+        char c4 = 'd';
+
+        String result = c1 + "" + i + " " + c2 + s + c3 + b + c4 + " " + f + " " + bool;
+
+        System.out.println(result);
+    }
+}
